dbe5cd7461338cafb414538aae4951b048796dd8
manchester1912 <manuelbastidasba@gmail.com>
2024-05-02 00:44:38 -0300

    Se realizaron ejercicios de 3 al 5

--- ejerciciosBucles/bucles.js
+++ ejerciciosBucles/bucles.js
@@ -1,33 +1,91 @@
 console.log("--- Ejercicio 1 ---")
 
-let numero = parseInt(prompt("Ingrese un Numero"))
+// let numero = parseInt(prompt("Ingrese un Numero"))
 
-for (let index = 1; index <= 10; index++) {
-  resultado = numero * index
-  if (numero === 0) {
-    console.log("Todo numero multiplicado por cero da cero")
-    break
-  } else if (numero >= 1) { console.log(numero + " por " + index + " es igual a " + resultado) }
-}
+// for (let index = 1; index <= 10; index++) {
+//   resultado = numero * index
+//   if (numero === 0) {
+//     console.log("Todo numero multiplicado por cero da cero")
+//     break
+//   } else if (numero >= 1) { console.log(numero + " por " + index + " es igual a " + resultado) }
+// }
 
 console.log("--- Ejercicio 2 ---")
 
 
-let contador = 0
-let acumulado = 0
+// let contador = 0
+// let acumulado = 0
 
-while (contador < 10) {
-  numero2 = parseInt(prompt("Ingrese un Numero del 0 al 9. Tiene diez chances"))
-  acumulado = acumulado + numero2
-  console.log("El Acumulado es " + acumulado)
-  contador++
-  console.log("Usted ha usado el chance " + contador + " de 10 disponibles" )
+// while (contador < 10) {
+//   numero2 = parseInt(prompt("Ingrese un Numero del 0 al 9. Tiene diez chances"))
+//   acumulado = acumulado + numero2
+//   console.log("El Acumulado es " + acumulado)
+//   contador++
+//   console.log("Usted ha usado el chance " + contador + " de 10 disponibles" )
 
-  if (numero2 === 0) {
-    console.log("Usted ha ingresado cero, el programa se detiene")
-    break
-  }
-}
+//   if (numero2 === 0) {
+//     console.log("Usted ha ingresado cero, el programa se detiene")
+//     break
+//   }
+// }
+
+
+console.log("--- Ejercicio 3 ---")
+
+// let numeroIncognita = 19
+
+// let contador = 1
+
+// while (contador <= 100) {
+//   let numeroIngresado = parseInt(prompt("Ingrese un numero del 1 al 100"))
+//   if (numeroIngresado === numeroIncognita) {console.log("Felicitaciones, haz adivinado en el intento " + contador)
+//      alert("FELICITACIONES !! Lograste adivinar en el intento " + contador)
+//     break
+
+//   }else if (numeroIngresado > numeroIncognita) {console.log("Haz Ingresado un numero Mayor, Vuelve a intentarlo")
+//   alert("Haz Ingresado un numero Mayor, Vuelve a intentarlo")
+//       } else {console.log("Haz ingresado un numero Menor, Vuelve a Intentarlo")
+//     alert("Haz Ingresado un numero Menor, Vuelve a intentarlo")
+//     }
+//    contador ++
+// }
+
+
+console.log("--- Ejercicio 4 ---")
+
+// let numero4 = parseInt(prompt("Ingrese un numero del 1 al 100"))
+// let contador = 0
+// for (let index = 1; index <= numero4; index++) {
+
+//   let resto = numero4 % index
+//   if (resto === 0) { contador++ }
+// }
+
+// if (contador === 2) {
+//   console.log(numero4 + " es un número Primo")
+// }
+// else { console.log(numero4 + " no es un número Primo")}
+
+
+console.log("--- Ejercicio 5 ---")
+
+
+// let numero5 = parseInt(prompt("Ingrese un numero del 1 al 100"))
+
+// for (let index = 1; index <= numero5; index++) {
+
+//   let resto = numero5 % index
+//   if (resto === 0) { console.log(index + " es uno de los divisores de " + numero5)}
+// }
+
+
+console.log("--- Ejercicio 6 ---")
+
+
+
+
+
+
 
 
 
